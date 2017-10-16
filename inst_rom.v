26151540210d7316511ffe45a5ed967685bd7e15
张钰晖 <yuhui-zh15@mails.tsinghua.edu.cn>
2017-10-16 16:51:47 +0800

    Complete inst_rom openmips

---
+++ inst_rom.v
@@ -0,0 +1,21 @@
+`include "defines.v"
+
+module inst_rom(
+    input wire ce,
+    input wire[`InstAddrBus] addr,
+    output reg[`InstBus] inst
+);
+
+    reg[`InstBus] inst_mem[0:`InstMemNum-1]
+
+    initial $readmemh ("inst_rom.data", inst_mem);
+
+    always @(*) begin
+        if (ce == `ChipDisable) begin
+            inst <= `ZeroWord; 
+        end else begin
+            inst <= inst_mem[addr[`InstMemNulLog2+1:2]]
+        end
+    end
+
+endmodule // inst_rom

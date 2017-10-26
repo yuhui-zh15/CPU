0cb96b79ed588f32c03a7bc1490d387d773713aa
张钰晖 <yuhui-zh15@mails.tsinghua.edu.cn>
2017-10-26 23:33:37 +0800

    Add Arithmetic Instruction

--- ex.v
+++ ex.v
@@ -56,7 +56,54 @@ module ex(
     assign reg1_lt_reg2 = ((aluop_i == `EXE_SLT_OP))? ((reg1_i[31] && !reg2_i[31]) || (!reg1_i[31] && !reg2_i[31] && result_sum[31]) || (reg1_i[31] && reg2_i[31] && result_sum[31])): (reg1_i < reg2_i);
     assign reg1_i_not = ~reg1_i;
 
-    
+    // Stage 2: Assign arithmeticres according to various arithmetic operation
+    always @(*) begin
+        if (rst == `RstEnable) begin
+            arithmeticres <= `ZeroWord; 
+        end else begin
+            case (aluop_i)
+                `EXE_SLT_OP, `EXE_SLTU_OP: begin
+                    arithmeticres <= reg1_lt_reg2; 
+                end
+                `EXE_ADD_OP, `EXE_ADDU_OP, `EXE_ADDI_OP, `EXE_ADDIU_OP: begin
+                    arithmeticres <= result_sum; 
+                end
+                `EXE_SUB_OP, `EXE_SUBU_OP: begin
+                    arithmeticres <= result_sum; 
+                end
+                `EXE_CLZ_OP: begin
+                    arithmeticres <= reg1_i[31] ?0 :reg1_i[30] ?1 :reg1_i[29] ?2 :reg1_i[28] ?3 :reg1_i[27] ?4 :reg1_i[26] ?5 :reg1_i[25] ?6 :reg1_i[24] ?7 :reg1_i[23] ?8 :reg1_i[22] ?9 :reg1_i[21] ?10 :reg1_i[20] ?11 :reg1_i[19] ?12 :reg1_i[18] ?13 :reg1_i[17] ?14 :reg1_i[16] ?15 :reg1_i[15] ?16 :reg1_i[14] ?17 :reg1_i[13] ?18 :reg1_i[12] ?19 :reg1_i[11] ?20 :reg1_i[10] ?21 :reg1_i[9] ?22 :reg1_i[8] ?23 :reg1_i[7] ?24 :reg1_i[6] ?25 :reg1_i[5] ?26 :reg1_i[4] ?27 :reg1_i[3] ?28 :reg1_i[2] ?29 :reg1_i[1] ?30 :reg1_i[0] ?31 :32;
+                end
+                `EXE_CLO_OP: begin
+                    arithmeticres <= reg1_i_not[31] ?0 :reg1_i_not[30] ?1 :reg1_i_not[29] ?2 :reg1_i_not[28] ?3 :reg1_i_not[27] ?4 :reg1_i_not[26] ?5 :reg1_i_not[25] ?6 :reg1_i_not[24] ?7 :reg1_i_not[23] ?8 :reg1_i_not[22] ?9 :reg1_i_not[21] ?10 :reg1_i_not[20] ?11 :reg1_i_not[19] ?12 :reg1_i_not[18] ?13 :reg1_i_not[17] ?14 :reg1_i_not[16] ?15 :reg1_i_not[15] ?16 :reg1_i_not[14] ?17 :reg1_i_not[13] ?18 :reg1_i_not[12] ?19 :reg1_i_not[11] ?20 :reg1_i_not[10] ?21 :reg1_i_not[9] ?22 :reg1_i_not[8] ?23 :reg1_i_not[7] ?24 :reg1_i_not[6] ?25 :reg1_i_not[5] ?26 :reg1_i_not[4] ?27 :reg1_i_not[3] ?28 :reg1_i_not[2] ?29 :reg1_i_not[1] ?30 :reg1_i_not[0] ?31 :32; 
+                end
+                default: begin
+                    arithmeticres <= `ZeroWord; 
+                end
+            endcase 
+        end
+    end
+
+    // Stage 3: Multiply Operation
+    assign opdata1_mult = (((aluop_i == `EXE_MUL_OP) || (aluop_i == `EXE_MULT_OP)) && (reg1_i[31]))? (~reg1_i + 1): reg1_i;
+    assign opdata2_mult = (((aluop_i == `EXE_MUL_OP) || (aluop_i == `EXE_MULT_OP)) && (reg2_i[31]))? (~reg2_i + 1): reg2_i;
+    assign hilo_tmp = opdata1_mult * opdata2_mult;
+    always @(*) begin
+        if (rst == `RstEnable) begin
+            mulres <= {`ZeroWord, `ZeroWord}; 
+        end else if ((aluop_i == `EXE_MULT_OP) || (aluop_i == `EXE_MUL_OP)) begin
+            if (reg1_i[31] ^ reg2_i[31]) begin
+                mulres <= ~hilo_temp + 1; 
+            end else begin
+                mulres <= hilo_temp; 
+            end
+        end else begin
+            mulres <= hilo_temp; 
+        end
+    end
+
+    // Stage 4: Determine data written, just modify following codes
+    // Stage 5: Determine HI/LO, just modify following codes
 
     // Logic
     always @(*) begin
@@ -151,6 +198,10 @@ module ex(
             whilo_o <= `WriteDisable;
             hi_o <= `ZeroWord;
             lo_o <= `ZeroWord; 
+        end else if ((aluop_i == `EXE_MULT_OP) || (aluop_i == `EXE_MULTU_OP)) begin
+            whilo_o <= `WriteEnable;  
+            hi_o <= mulres[63:32];
+            lo_o <= mulres[31:0];
         end else if (aluop_i == `EXE_MTHI_OP) begin
             whilo_o <= `WriteEnable;
             hi_o <= reg1_i;
@@ -168,7 +219,11 @@ module ex(
 
     always @(*) begin
         wd_o <= wd_i;
-        wreg_o <= wreg_i;
+        if (((aluop_i == `EXE_ADD_OP) || (aluop_i == `EXE_ADDI_OP) || (aluop_i == `EXE_SUB_OP)) && (ov_sum)) begin
+            wreg_o <= `WriteDisable;
+        end else begin
+            wreg_o <= wreg_i;
+        end
         case (alusel_i)
             `EXE_RES_LOGIC: begin
                 wdata_o <= logicout;  
@@ -178,6 +233,12 @@ module ex(
             end
             `EXE_RES_MOVE: begin
                 wdata_o <= moveres; 
+            end
+            `EXE_RES_ARITHMETIC: begin
+                wdata_o <= arithmeticres; 
+            end
+            `EXE_RES_MUL: begin
+                wdata_o <= mulres[31: 0];
             end
             default: begin
                 wdata_o <= `ZeroWord;  

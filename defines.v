567fa7668b6da1eb0781779f7e50307b381066a9
张钰晖 <yuhui-zh15@mails.tsinghua.edu.cn>
2017-10-27 17:30:39 +0800

    Modify define.v

--- defines.v
+++ defines.v
@@ -83,6 +83,16 @@
 `define EXE_BLTZ  5'b00000
 `define EXE_BNE   6'b000101
 
+// Instruction Load/Store
+`define EXE_LB 6'b100000
+`define EXE_LBU 6'b100100
+`define EXE_LH 6'b100001
+`define EXE_LHU 6'b100101
+`define EXE_LW 6'b100011
+`define EXE_SB 6'b101000
+`define EXE_SH 6'b101001
+`define EXE_SW 6'b101011
+
 // ALU Operation
 `define EXE_AND_OP 8'b00100100
 `define EXE_OR_OP 8'b00100101

3be8c5bfdf54947158f171dc8aa93b57c479ad3b
yuhui <zyh.thu@gmail.com>
2017-12-21 22:07:28 +0800

    modify ucore and pass all tests

--- ucore/kern/mm/memlayout.h
+++ ucore/kern/mm/memlayout.h
@@ -6,9 +6,9 @@
 #define KERNBASE            0x80000000
 
 #ifdef MACH_FPGA            
-#define KMEMSIZE            (1 << 20)
+#define KMEMSIZE            (8 << 20)
 #else
-#define KMEMSIZE            (32 << 20)                 // 512M the maximum amount of physical memory
+#define KMEMSIZE            (8 << 20)                 // 512M the maximum amount of physical memory
 #endif
 
 #define KERNTOP             (KERNBASE + KMEMSIZE)

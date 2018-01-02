4c5721fdec031a9f62ddf52bcf9d2696a7930e33
周正平 <zhouzp15@mails.tsinghua.edu.cn>
2018-01-03 01:20:34 +0800

    add ppt software, add latest bitstream, add latest ucore

---
+++ tools/image/ppt.c
@@ -0,0 +1,18 @@
+#include <ulib.h>
+#include <stdio.h>
+#include "ppt1.h"
+extern uint8_t ppt1[];
+#define VGA 0xba000000
+
+int main() {
+    cprintf("This is our ppt software\n");
+    uint32_t port = VGA;
+    int i;
+    int j;
+    for (i = 0; i < 600; i++) {
+        for (j = 0; j < 800; j++) {
+            *((volatile uint8_t*) (port+i*800+j))  = ppt1[i+j*600];
+        }
+    }
+    return 0;
+}
